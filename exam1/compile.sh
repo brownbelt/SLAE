ba6ec7ddf31a74bb5da4cc202e1306cf08b30dea
arno01 <andrey.arapov@gmail.com>
2013-03-09 11:47:19 +0100

    minor updates

--- exam1/compile.sh
+++ exam1/compile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 ARG1=$1
 set -x
-nasm -f elf32 -o $ARG1.o $ARG1.nasm && ld -m elf_i386 -o $ARG1 $ARG1.o
+nasm -f elf32 -o $ARG1.o $ARG1.nasm && ld -m elf_i386 -o $ARG1 $ARG1.o \
+&& set +x && echo "Shellcode: " && objdump -d ./$1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-7 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g'
 
